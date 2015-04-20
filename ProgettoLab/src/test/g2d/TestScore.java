87eb1f14ce07dd27c09f95758357f2c6ba63f341
bsangiovanni <bianca.s1393@gmail.com>
2015-04-20 18:24:09 +0200

    test per lo score calculator

---
+++ ProgettoLab/src/test/g2d/TestScore.java
@@ -0,0 +1,14 @@
+package test.g2d;
+
+import model.ScoreCalculator;
+
+public class TestScore {
+	
+	public static void main(String[] args) {
+		
+		ScoreCalculator score = new ScoreCalculator();
+		score.start();
+
+	}
+
+}
