a70f194255b629c7967cdc6149b08ee51971dd76
gboi <giulia93boi@gmail.com>
2015-04-21 22:44:38 +0200

    A SVN piace UN SACCO rendere le cose complicate quindi mi tocca fare in due passaggi...

---
+++ ProgettoLab/src/model/scores/ScoreCalculator.java
@@ -0,0 +1,31 @@
+package model.scores;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class ScoreCalculator {
+	
+	private long score = 0;
+	private static final int COEFF = 50;
+	
+	
+	public void start(){
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+
+				score = score+COEFF;
+				System.out.println(score);
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 500);
+	}
+
+	public long getScore() {
+		return score;
+	}
+
+}
